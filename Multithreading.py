da0ecc501c395961c310c5637e78d6c5d6631b8b
Thuthukani.Langa <Thuthukani57@gmail.com>
2024-03-27 13:24:58 +0200

    Encrypts and Decrypts text files.

---
+++ Multithreading.py
@@ -0,0 +1,15 @@
+import threading
+import time
+
+def print_numbers():
+    for i in range(5):
+        print(i)
+        time.sleep(1)
+
+thread = threading.Thread(target=print_numbers)
+
+thread.start()
+
+for _ in range(3):
+    print("Main thread is working...")
+    time.sleep(1)
